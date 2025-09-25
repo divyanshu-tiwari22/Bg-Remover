4571ddafac323762e99a2a24bc60b57585ab7f30
divyanshu-tiwari22 <thebatman1459@gmail.com>
2025-09-25 22:57:09 +0530

    Integrated Clerk in frontend again

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,6 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
-
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
